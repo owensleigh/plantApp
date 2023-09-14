36cca473def0354a252e327fa4cf0813c3cb483d
owensleigh <owen.b.sleigh@gmail.com>
2023-09-14 15:11:22 -0700

    Initial Commit

---
+++ PlantGrowinator/PlantGrowinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantGrowinatorApp.swift
+//  PlantGrowinator
+//
+//  Created by Owen Sleigh on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct PlantGrowinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
